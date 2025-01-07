568c6554534fadd0953542401f05205ee8bf32da
kylew2001 <kswalker2201@gmail.com>
2025-01-07 17:34:31 +1300

    Initial Commit

---
+++ main.py
@@ -0,0 +1,41 @@
+
+
+
+# first_name = "Kyle"  #String is a word
+# last_name = "Walker"
+# full_name = first_name + " " + last_name
+#
+# print(full_name)type(first_name)
+#
+# #print("Hello " + name)
+#
+# #age = 22  #Int is a whole Number
+# #age += 1
+# #print("Your age is: " + str(age))
+# #print(type(age))
+#
+# #height = 250.5   #Float is a Decimal
+# #print("Your height is: "+str(height)+"cm")
+#
+# human = True #Boolean is true/false
+# print("Are you a human?: "+str(human))
+
+
+# name="Kyle"
+# age=21
+# attractive=True
+
+# name, age, attractive = "Kyle", 21, True
+#
+# print(name)
+# print(age)
+# print(attractive)
+
+# Spongebob = 30
+# Patrick = 30
+# Sandy = 30
+# Squidward = 30
+
+Spongebob = Patric = Sandy = Squidward = 30
+
+print(Spongebob)
